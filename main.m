7f9cd87708c3cded49449472c2baef9218bf0c4f
Honghong-Wang <54888488+Honghong-Wang@users.noreply.github.com>
2024-07-19 19:45:25 +0800

    Update main.m

--- main.m
+++ main.m
@@ -5,61 +5,31 @@
 % available at: https://hal.science/hal-03935561/ .
 %
 
+function write_scenario_results(fid, scenario_name, position_error_median_mm, position_error_95CI_mm, orientation_error_median_deg, orientation_error_95CI_deg)
+    fprintf(fid, '\nScenario %s\n----------\n', scenario_name);
+    fprintf(fid, 'position [mm]:\n');
+    fprintf(fid, '   - median:     %.2e\n', position_error_median_mm);
+    fprintf(fid, '   - low 95 CI:  %.2e\n', position_error_95CI_mm(1));
+    fprintf(fid, '   - high 95 CI: %.2e\n', position_error_95CI_mm(2));
+    fprintf(fid, 'orientation [deg]:\n');
+    fprintf(fid, '   - median:     %.2e\n', orientation_error_median_deg);
+    fprintf(fid, '   - low 95 CI:  %.2e\n', orientation_error_95CI_deg(1));
+    fprintf(fid, '   - high 95 CI: %.2e\n\n', orientation_error_95CI_deg(2));
+end
+
 fid = fopen('results_output.txt', 'w');
 
-mainA;
-fprintf(fid, '\nScenario A\n----------\n');
-fprintf(fid, 'position [mm]:\n');
-fprintf(fid, '   - median:     %.2e\n',position_error_median_mm);
-fprintf(fid, '   - low 95 CI:  %.2e\n',position_error_95CI_mm(1));
-fprintf(fid, '   - high 95 CI: %.2e\n',position_error_95CI_mm(2));
-fprintf(fid, 'orientation [deg]:\n');
-fprintf(fid, '   - median:     %.2e\n',orientation_error_median_deg);
-fprintf(fid, '   - low 95 CI:  %.2e\n',orientation_error_95CI_deg(1));
-fprintf(fid, '   - high 95 CI: %.2e\n\n',orientation_error_95CI_deg(2));
+scenarios = {'A', 'B', 'C', 'D', 'E'};
 
-mainB;
-fprintf(fid, '\nScenario B\n----------\n');
-fprintf(fid, 'position [mm]:\n');
-fprintf(fid, '   - median:     %.2e\n',position_error_median_mm);
-fprintf(fid, '   - low 95 CI:  %.2e\n',position_error_95CI_mm(1));
-fprintf(fid, '   - high 95 CI: %.2e\n',position_error_95CI_mm(2));
-fprintf(fid, 'orientation [deg]:\n');
-fprintf(fid, '   - median:     %.2e\n',orientation_error_median_deg);
-fprintf(fid, '   - low 95 CI:  %.2e\n',orientation_error_95CI_deg(1));
-fprintf(fid, '   - high 95 CI: %.2e\n\n',orientation_error_95CI_deg(2));
-
-mainC;
-fprintf(fid, '\nScenario C\n----------\n');
-fprintf(fid, 'position [mm]:\n');
-fprintf(fid, '   - median:     %.2e\n',position_error_median_mm);
-fprintf(fid, '   - low 95 CI:  %.2e\n',position_error_95CI_mm(1));
-fprintf(fid, '   - high 95 CI: %.2e\n',position_error_95CI_mm(2));
-fprintf(fid, 'orientation [deg]:\n');
-fprintf(fid, '   - median:     %.2e\n',orientation_error_median_deg);
-fprintf(fid, '   - low 95 CI:  %.2e\n',orientation_error_95CI_deg(1));
-fprintf(fid, '   - high 95 CI: %.2e\n\n',orientation_error_95CI_deg(2));
-
-mainD;
-fprintf(fid, '\nScenario D\n----------\n');
-fprintf(fid, 'position [mm]:\n');
-fprintf(fid, '   - median:     %.2e\n',position_error_median_mm);
-fprintf(fid, '   - low 95 CI:  %.2e\n',position_error_95CI_mm(1));
-fprintf(fid, '   - high 95 CI: %.2e\n',position_error_95CI_mm(2));
-fprintf(fid, 'orientation [deg]:\n');
-fprintf(fid, '   - median:     %.2e\n',orientation_error_median_deg);
-fprintf(fid, '   - low 95 CI:  %.2e\n',orientation_error_95CI_deg(1));
-fprintf(fid, '   - high 95 CI: %.2e\n\n',orientation_error_95CI_deg(2));
-
-mainE;
-fprintf(fid, '\nScenario E\n----------\n');
-fprintf(fid, 'position [mm]:\n');
-fprintf(fid, '   - median:     %.2e\n',position_error_median_mm);
-fprintf(fid, '   - low 95 CI:  %.2e\n',position_error_95CI_mm(1));
-fprintf(fid, '   - high 95 CI: %.2e\n',position_error_95CI_mm(2));
-fprintf(fid, 'orientation [deg]:\n');
-fprintf(fid, '   - median:     %.2e\n',orientation_error_median_deg);
-fprintf(fid, '   - low 95 CI:  %.2e\n',orientation_error_95CI_deg(1));
-fprintf(fid, '   - high 95 CI: %.2e\n\n',orientation_error_95CI_deg(2));
+for i = 1:length(scenarios)
+    scenario = scenarios{i};
+    try
+        feval(['main', scenario]); % Call the main function for the current scenario
+        write_scenario_results(fid, scenario, position_error_median_mm, position_error_95CI_mm, orientation_error_median_deg, orientation_error_95CI_deg);
+    catch ME
+        fprintf(fid, '\nScenario %s encountered an error: %s\n', scenario, ME.message);
+    end
+end
 
 fclose(fid);
+
